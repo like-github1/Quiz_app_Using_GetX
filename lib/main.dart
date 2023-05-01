128df1b7068a43b82683153d57865ff90676aa47
solomon girma <likeprogramers@gmail.com>
2023-05-01 10:10:59 +0300

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/screens/quiz/quiz_screen.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: QuizScreen(), 
+      //WelcomeScreen(),
+    );
+  }
+}
